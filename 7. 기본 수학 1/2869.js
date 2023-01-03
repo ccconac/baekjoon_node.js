1d0e3cc1c37647119a8e78d246f9d3fc817a4111
ccconac <qwerpoiu91@naver.com>
2023-01-03 18:10:43 +0900

    node.js 기본 수학 1

---
+++ 7. 기본 수학 1/2869.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync(__dirname + '/textfile.txt').toString().split(' ');
+
+const [A, B, V] = [input[0], input[1], input[2]].map(Number);
+
+let day = Math.ceil((V - B) / (A - B));
+console.log(day);
+
+// 1. 시간 초과
+// while (true) {
+//     h += A - B;
+//     day += 1;
+//     if (V <= h + A) {
+//         h += A;
+//         day += 1;
+//         break;
+//     }
+// }
+
+// console.log(day);
+
+
