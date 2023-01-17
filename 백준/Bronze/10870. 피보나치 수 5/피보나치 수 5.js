e6e0d56cbcfc8b98528c449b91780bd4cdd11db0
Moon SoHee <106158901+ccconac@users.noreply.github.com>
2023-01-17 18:09:22 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 172 ms, Memory: 9672 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -0,0 +1,9 @@
+const n = require('fs').readFileSync(0);
+
+function f(n) {
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else return f(n - 1) + f(n - 2); 
+}
+
+console.log(f(n));
