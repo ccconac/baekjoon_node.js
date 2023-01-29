8dee167d55b3838a3c8c797725a049bf41028f76
Moon SoHee <106158901+ccconac@users.noreply.github.com>
2023-01-30 04:34:25 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 120 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.js
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.js
@@ -1,14 +1,3 @@
 const input = require('fs').readFileSync(0).toString();
 
-let result = 0;
-
-if (input >= 5) {
-    result += Math.floor(input / 5);
-}
-
-if (input % 5 === 0) {
-    console.log(result);
-} else {
-    console.log(result + 1);
-}
-
+console.log(Math.ceil(input / 5));
